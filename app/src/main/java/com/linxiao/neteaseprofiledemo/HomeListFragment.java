60eddbf26e5aaff2ce61e1a91bb88a998644c97a
LinXiao <tsunamibad@qq.com>
2017-02-23 10:57:20 +0800

    update gradle files,

--- app/src/main/java/com/linxiao/neteaseprofiledemo/HomeListFragment.java
+++ app/src/main/java/com/linxiao/neteaseprofiledemo/HomeListFragment.java
@@ -25,16 +25,9 @@ import butterknife.ButterKnife;
  * @version 1.0.0
  */
 public class HomeListFragment extends ScrollObservableFragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
     private int scrolledX = 0, scrolledY = 0;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private View contentView;
 
@@ -45,20 +38,9 @@ public class HomeListFragment extends ScrollObservableFragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeListFragment newInstance(String param1, String param2) {
+    public static HomeListFragment newInstance() {
         HomeListFragment fragment = new HomeListFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -66,10 +48,6 @@ public class HomeListFragment extends ScrollObservableFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
